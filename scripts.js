680c068cae06f2c6bad1bb154691ef2062fbf71a
Farheen Malik <farheen.m.malik@gmail.com>
2016-05-27 11:08:03 -0400

    Decided to use box version of map, now going to transfer over all info

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function createSelector(layer, selector, dataset, col) {
-	var sql = new cartodb.SQL({ user: 'fma2' });
-	var $options = $(selector);
-	$options.click(function(e) {
+  var sql = new cartodb.SQL({ user: 'fma2' });
+  var $options = $(selector);
+  $options.click(function(e) {
     // get the area of the selected layer
     var $li = $(e.target);
     var filter = $li.attr('data');
@@ -14,9 +14,10 @@ function createSelector(layer, selector, dataset, col) {
     // create query based on data from the layer
     var query = "select * from " + dataset;
 
-    if(filter !== 'all') {
-      query = "select * from " + dataset + " where " + col + filter;
-    }
+
+    // if(filter !== 'all') {
+    //   query = "select * from " + dataset + " where " + col + filter;
+    // }
 
     if (type === "cartocss") {
       condition = $('#'+param).text();
@@ -32,23 +33,23 @@ function createSelector(layer, selector, dataset, col) {
 }
 
 function main() {
-	cartodb.createVis('map', 'https://fma2.cartodb.com/api/v2/viz/509bcd20-f503-11e5-a235-0ea31932ec1d/viz.json', 
-	{
+  cartodb.createVis('map', 'https://fma2.cartodb.com/api/v2/viz/a2d6d490-0282-11e6-9390-0e31c9be1b51/viz.json', 
+  {
     shareable: true,
     tiles_loader: true,
-    zoom: 4
+    zoom: 6
     // center_lat: 0,
     // center_lon: 0,
     // title: true,
     // description: true,
     // search: true,
   })
-	.done(function(vis, layers) {
+  .done(function(vis, layers) {
     // layer 0 is the base layer, layer 1 is cartodb layer
     // setInteraction is disabled by default
     layers[1].setInteraction(true);
     // layers[1].on('featureOver', function(e, latlng, pos, data) {
-    // 	cartodb.log.log(e, latlng, pos, data);
+    //  cartodb.log.log(e, latlng, pos, data);
     // });
     
     // you can get the native map to work with it
@@ -89,7 +90,7 @@ function main() {
       // create selector for this layer
       createSelector(subLayer, '#layer_selector li.poverty-rate', 'us_states_with_ak_hi_carto_db_rtw_assets_opp_with_geo', 'income_povery_rate_percent');
     }) 
-    cartodb.createLayer(map,'https://fma2.cartodb.com/api/v2/viz/8c2ba0e6-faa2-11e5-bc96-0e31c9be1b51/viz.json')
+    cartodb.createLayer(map,'https://fma2.cartodb.com/api/v2/viz/cfefc586-0318-11e6-9de5-0ea31932ec1d/viz.json')
     .addTo(map)
     .done(function(layer){
       var subLayer = layer.getSubLayer(0);
@@ -98,12 +99,12 @@ function main() {
       subLayer.hide();
 
       // create selector for this layer
-      createSelector(subLayer, '#layer_selector li.min-wage', 'us_states_with_ak_hi_carto_db_rtw_assets_opp_with_geo', 'min_wage');
+      createSelector(subLayer, '#layer_selector li.min-wage', 'state_squares_rtw_states_data', 'min_wage');
     })  
   })
-	.error(function(err) {
-		console.log(err);
-	});
+  .error(function(err) {
+    console.log(err);
+  });
 }
 
 $(document).ready(function() {
